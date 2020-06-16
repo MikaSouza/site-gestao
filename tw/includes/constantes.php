0361f854a2121fa9fe996f1a4fa32e83ea92e301
JordanoTI <jordano.rodrigues@teraware.com.br>
2020-06-16 18:19:01 -0300

    Alterações by Jordano

--- tw/includes/constantes.php
+++ tw/includes/constantes.php
@@ -2,14 +2,14 @@
 	session_start();
 	header('Content-Type: text/html; charset=utf-8');
 
-	define('db_database', 'tweduc02');
-	define('db_user', 'tweduc02');
-	define('db_pass', '9JG2cElu');
-	define('db_host', 'mysql.tweduc.com.br');
+	define('db_database', 'teraware01');
+	define('db_user', 'teraware01');
+	define('db_pass', '0bZDmKcgWA');
+	define('db_host', 'mysql.teraware.net.br');
 
-	define('cSNomeEmpresa', 'TWEduc Ltda');
+	define('cSNomeEmpresa', 'Gestão Ltda');
 	define('cSUrlSiteEmpresa', 'http://sites-gestao-srv.teraware.net.br/');
-	define('cSSegmentoAtendimento', 'TWEduc Ltda');
+	define('cSSegmentoAtendimento', 'Gestão Ltda');
 	define('cSTelefone1', '+55 (51) 3061 2550');
 	define('cSTelefone2', '+55 (51) 3061 1315');
 	define('cSTelefone3', '');
